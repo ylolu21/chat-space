fa5591e6cbcd8f263b90f098be02be5f0994d118
ylolu21 <ylolu21@gmail.com>
2018-07-19 21:38:51 +0900

    Implement posting function

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ApplicationRecord
-	has_many :users, through: :groups_users
-	has_many :group_users
-	has_many :messages
-	accepts_nested_attributes_for :group_users
+  has_many :group_users
+  has_many :users, through: :group_users
+  validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-	has_many :groups, through: :group_users
-	has_many :group_users
-	has_many :messages
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  has_many :messages
+  has_many :group_users
+  has_many :groups, through: :group_users
 end

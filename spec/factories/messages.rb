d12ad3675ea7a4cfe76f2e7ff2bcb56a73b75168
ylolu21 <ylolu21@gmail.com>
2018-09-02 19:51:40 +0900

    modify_something

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/images/mOT001.jpg")
+    image File.open("#{Rails.root}/public/images/no_image.jpg")
     user
     group
   end

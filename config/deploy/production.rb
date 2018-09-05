b10f61245a2fe4c9cfc34efbd32cd1a97f943702
ylolu21 <ylolu21@gmail.com>
2018-09-05 19:18:44 +0900

    Modify capfile etc.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '54.168.118.151', user: 'ec2-user', roles: %w{app db web}
 
 # role-based syntax
 # ==================
@@ -59,3 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+

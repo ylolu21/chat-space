221019293cc9221f2c2ddd989c0c0babf58f5843
ylolu21 <ylolu21@gmail.com>
2018-09-09 17:41:01 +0900

    Unnecessary spacing delete

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'ys-chat-space'
+  config.fog_directory = 'ys-chat-space'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ys-chat-space'
 end
